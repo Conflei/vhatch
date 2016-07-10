d047d2c2a6570beb114905da58adcd64ac031e61
Conflei <Felix1645018>
2016-07-10 19:09:38 -0430

    back 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
-  before_action :link_params,only: [:create]
+  before_action :user_params,only: [:create]
 
   def index
     @users = User.all
